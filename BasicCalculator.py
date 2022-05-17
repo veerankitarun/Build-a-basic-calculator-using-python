a6afaee12de1b25873ca3a92fa1a2218515dfd37
Tarun Kumar Goud <61703896+veerankitarun@users.noreply.github.com>
2022-05-17 07:31:52 +0530

    Update BasicCalculator.py

--- BasicCalculator.py
+++ BasicCalculator.py
@@ -1,3 +1,7 @@
+# you are going to a make the basic calculator using python code 
+# just download the code and run you just need to change the file name extension as .py
+
+
 def add(P, Q):
     # This function is used for adding two numbers
     return P + Q
@@ -33,7 +37,7 @@ num_2 = int(input("Please enter the second number: "))
 if choice == 'a':
     print(num_1, " + ", num_2, " = ", add(num_1, num_2))
 
-elif choice == 'b':
+elif choice == 'b':  # elif is like if-else statement 
     print(num_1, " - ", num_2, " = ", subtract(num_1, num_2))
 
 elif choice == 'c':
@@ -41,4 +45,4 @@ elif choice == 'c':
 elif choice == 'd':
     print(num_1, " / ", num_2, " = ", divide(num_1, num_2))
 else:
-    print("This is an invalid input")
+    print("enter a valid input")  # if any input don't match with the given values then the output will be invalid
